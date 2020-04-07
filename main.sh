818b461795923146da70ecc6b72815f7b2816f72
killDevils <55968229+killDevils@users.noreply.github.com>
2020-04-07 15:08:46 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,8 +2,6 @@
 
 read -p $'\e[1;44m Type in folder name under /var/www/: \e[0m' folderName
 
-sudo apt install php7.2 libapache2-mod-php7.2 php7.2-common php7.2-mysql php7.2-gmp php7.2-curl php7.2-intl php7.2-mbstring php7.2-xmlrpc php7.2-gd php7.2-bcmath php7.2-soap php7.2-ldap php7.2-imap php7.2-xml php7.2-cli php7.2-zip -y
-
 sudo apt install unzip -y
 wget https://jaist.dl.sourceforge.net/project/yetiforce/YetiForce%20CRM%205.x.x/5.3.0/YetiForceCRM-5.3.0-complete.zip
 unzip YetiForce*.zip -d /var/www/$folderName
